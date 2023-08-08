ec57afc7c5eea1ac134667d4ce4351ecbfaabcd4
Yousif Abozid <yousif.abozid@yahoo.com>
2023-08-08 19:40:24 +0300

    add 0x01. Python - if-else, loops, functions task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(len(str)):
-        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            str[i] = chr(ord(str[i]) - 32)
-    print("{}".format(str), end="")
+    for c in range(len(str)):
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
