b8f9f5512f911c0028b8ed3f5b0fb549599c9e8f
Yousif Abozid <yousif.abozid@yahoo.com>
2023-08-11 01:29:04 +0300

    add 0x02. Python - import & modules task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,25 +4,21 @@ import sys
 from calculator_1 import add, div, mul, sub
 
 if __name__ == "__main__":
-    length = len(sys.argv)
-    argv = sys.argv
-    a = int(argv[1])
-    b = int(argv[3])
-    operator = argv[2]
-    result = 0
-    if length == 4:
-        if operator == "+":
-            result = add(a, b)
-        elif operator == "-":
-            result = sub(a, b)
-        elif operator == "*":
-            result = mul(a, b)
-        elif operator == "/":
-            result = div(a, b)
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
-        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
-    else:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
+
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+    if operator == "+":
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif operator == "-":
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif operator == "*":
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif operator == "/":
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
