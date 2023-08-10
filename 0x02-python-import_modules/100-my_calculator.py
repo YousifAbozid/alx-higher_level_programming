46e96bf04e6c38176e4210d8ace5aeab21fbe5de
Yousif Abozid <yousif.abozid@yahoo.com>
2023-08-11 01:26:21 +0300

    add 0x02. Python - import & modules task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,22 +6,23 @@ from calculator_1 import add, div, mul, sub
 if __name__ == "__main__":
     length = len(sys.argv)
     argv = sys.argv
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = argv[2]
     result = 0
     if length == 4:
-        if argv[2] == "+":
-            result = add(int(argv[1]), int(argv[3]))
-        elif argv[2] == "-":
-            result = sub(int(argv[1]), int(argv[3]))
-        elif argv[2] == "*":
-            result = mul(int(argv[1]), int(argv[3]))
-        elif argv[2] == "/":
-            result = div(int(argv[1]), int(argv[3]))
+        if operator == "+":
+            result = add(a, b)
+        elif operator == "-":
+            result = sub(a, b)
+        elif operator == "*":
+            result = mul(a, b)
+        elif operator == "/":
+            result = div(a, b)
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
-        print(
-            "{:d} {:s} {:d} = {:d}".format(int(argv[1]), argv[2],
-                                           int(argv[3]), result))
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
     else:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
