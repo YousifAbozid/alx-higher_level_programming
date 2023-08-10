bba18c036fb0520ef972232e5fff364ce2ae942d
Yousif Abozid <yousif.abozid@yahoo.com>
2023-08-11 01:18:54 +0300

    add 0x02. Python - import & modules task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+
+from calculator_1 import add, div, mul, sub
+
+if __name__ == "__main__":
+    length = len(sys.argv)
+    argv = sys.argv
+    result = 0
+    if length == 4:
+        if argv[2] == "+":
+            result = add(int(argv[1]), int(argv[3]))
+        elif argv[2] == "-":
+            result = sub(int(argv[1]), int(argv[3]))
+        elif argv[2] == "*":
+            result = mul(int(argv[1]), int(argv[3]))
+        elif argv[2] == "/":
+            result = div(int(argv[1]), int(argv[3]))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+        print(
+            "{:d} {:s} {:d} = {:d}".format(int(argv[1]), argv[2], int(argv[3]), result)
+        )
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
