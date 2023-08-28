5aed551ad8789ebed7aea95cf97890fc9445ff44
Yousif Abozid <yousif.abozid@yahoo.com>
2023-08-28 23:09:54 +0300

    add 0x05-python-exceptions task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as e:
-        print(e, file=sys.stderr)
+        sys.stderr.write("Exception: {}\n".format(e))
         return False
